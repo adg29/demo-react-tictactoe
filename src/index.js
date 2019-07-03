058d92f6e050e7237d9ad1b0ce381f491e097e4f
Alan Garcia <alandavidgarcia@gmail.com>
2019-07-03 00:26:14 -0400

    add stepNumber to the Game component’s state to indicate which step we’re currently viewing. efine the jumpTo method in Game to update that stepNumber. We also set xIsNext to true if the number that we’re changing stepNumber to is even

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
@@ -73,6 +74,13 @@ class Game extends React.Component {
                 squares: squares
             }]),
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
 
@@ -106,7 +114,7 @@ class Game extends React.Component {
             <div className="game-board">
                 <Board
                     squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
+                    onClick={(i)    => this.handleClick(i)}
                 />
             </div>
             <div className="game-info">
