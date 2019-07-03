fc85e1647148a307745ab6ca464a16b62f2cca41
Alan Garcia <alandavidgarcia@gmail.com>
2019-07-03 00:29:37 -0400

    update stepNumber by adding stepNumber: history.length as part of the this.setState argument in the handle click method. replace reading this.state.history with this.state.history.slice(0, this.state.stepNumber + 1). This ensures that if we go back in time and then make a new move from that point, we throw away all the future history that would now become incorrect.

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -73,6 +73,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
         });
     }
