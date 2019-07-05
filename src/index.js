d1b4a4d04f8fba83e14db837065000d8740e34d4
Alan Garcia <alandavidgarcia@gmail.com>
2019-07-05 14:14:57 -0400

    convert Game component into a function component using hooks. no longer need to use this.state.history. Instead, we treat history like a regular variable, because that’s what it is now. usestate converts Game into a function component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -47,75 +47,63 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null)
-            }],
-            stepNumber: 0,
-            xIsNext: true
-        };
-    }
+const Game = () => {
+    const [history, setHistory] = useState([{
+        squares: Array(9).fill(null)
+    }]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(true);
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
+    const currentBoard = history[stepNumber];
+    const winner = calculateWinner(currentBoard.squares);
+
+    const handleClick = (i) => {
+        const newHistory = history.slice(0, stepNumber + 1);
+        const squares = currentBoard.squares.slice();
 
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
 
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
-        });
+        squares[i] = xIsNext ? 'X' : 'O';
+
+        setHistory(newHistory.concat([{squares: squares}]));
+        setStepNumber(newHistory.length);
+        setXIsNext(!xIsNext);
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0
-        });
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
     }
 
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
 
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                `Go to move # ${move}` :
-                `Go to game start`;
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>
-                        {desc}
-                    </button>
-                </li>
-            );
-        });
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            `Go to move # ${move}` :
+            `Go to game start`;
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>
+                    {desc}
+                </button>
+            </li>
+        );
+    });
 
-        let status;
-        if (winner) {
-            status = `Winner: ${winner}`;
-        } else {
-            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-        }
+    let status;
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
         <div className="game">
             <div className="game-board">
                 <Board
-                    squares={current.squares}
-                    onClick={(i)    => this.handleClick(i)}
+                    squares={currentBoard.squares}
+                    onClick={(i)    => handleClick(i)}
                 />
             </div>
             <div className="game-info">
@@ -124,7 +112,6 @@ class Game extends React.Component {
             </div>
         </div>
     );
-    }
 }
 
 function calculateWinner(squares) {
